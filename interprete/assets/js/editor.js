e91478743856617ebd255c2e2abb1dbf009e7750
Aldo Vasquez <aldovasquez2014@gmail.com>
2023-08-07 10:09:13 -0600

    obteniendo datos del editor

---
+++ interprete/assets/js/editor.js
@@ -0,0 +1,91 @@
+const botonAbrirArchivo = document.getElementById("btn-abrir-archivo");
+const botonCrearArchivo = document.getElementById("btn-crear-archivo");
+const botonGuardarArchivo = document.getElementById("btn-guardar-archivo");
+const btnEjecutar = document.getElementById("btn-ejecutar");
+
+/*funcionalidad de editor de codigo*/
+
+var Fuente = CodeMirror.fromTextArea(document.getElementById("fuente"), {
+  mode: "text/x-java",
+  theme: "dracula",
+  lineNumbers: true,
+  autoCloseTags: true,
+});
+Fuente.setSize("600", "579");
+
+var Salida = CodeMirror.fromTextArea(document.getElementById("salida"), {
+  mode: "text/x-java",
+  theme: "dracula",
+  lineNumbers: true,
+  autoCloseTags: true,
+});
+Salida.setSize("600", "579");
+
+/*acciones de los botones de la barra de menus*/
+
+botonCrearArchivo.addEventListener("click", function () {
+  Fuente.setValue("");
+});
+
+botonAbrirArchivo.addEventListener("click", function () {
+  const input = document.createElement("input");
+  input.type = "file";
+  input.accept = ".tw";
+  input.onchange = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.readAsText(file, "UTF-8");
+    reader.onload = (readerEvent) => {
+      const content = readerEvent.target.result;
+      Fuente.setValue(content);
+    };
+  };
+  input.click();
+});
+
+botonGuardarArchivo.addEventListener("click", function () {
+  const fileName = window.prompt("Nombre del archivo:");
+  if (fileName) {
+    const textToSave = Fuente.getValue();
+    const textToSaveAsBlob = new Blob([textToSave], { type: "text/plain" });
+    const textToSaveAsURL = window.URL.createObjectURL(textToSaveAsBlob);
+    const downloadLink = document.createElement("a");
+    downloadLink.download = fileName + ".tw";
+    downloadLink.innerHTML = "Descargar archivo";
+    downloadLink.href = textToSaveAsURL;
+    downloadLink.onclick = destroyClickedElement;
+    downloadLink.style.display = "none";
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+  }
+});
+
+function destroyClickedElement(event) {
+  document.body.removeChild(event.target);
+}
+
+//funcionalidad para ejecutar archivo
+btnEjecutar.addEventListener("click", function () {
+  const codigo = Fuente.getValue();
+
+  fetch("/enviar-texto", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ contenido: codigo }),
+  })
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Error al obtener datos del backend")
+    }
+    return response.json();
+  })
+  .then((data) => {
+    // Aquí puedes procesar la respuesta del backend, si es necesario
+    console.log("Respuesta del backend:", data);
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
+});
--- interprete/main.go
+++ interprete/main.go
@@ -1,9 +1,36 @@
-package interprete
+package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
+type CodigoEnviado struct {
+	Contenido string `json:"contenido"`
+}
+
+func manejarEnviarcodigo(w http.ResponseWriter, r *http.Request) {
+	// Decodificar el cuerpo JSON en una estructura CodigoEnviado
+	var codigo CodigoEnviado
+	err := json.NewDecoder(r.Body).Decode(&codigo)
+	if err != nil {
+		http.Error(w, "Error al leer el contenido JSON", http.StatusBadRequest)
+		return
+	}
+
+	// Aquí puedes hacer lo que necesites con el contenido del textarea.
+	// Por ejemplo, imprimirlo en la consola.
+	fmt.Println("codigo recibido:", codigo.Contenido)
+
+	// Puedes enviar una respuesta al cliente si lo deseas.
+	respuesta := map[string]string{"mensaje": "Codigo recibido correctamente"}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(respuesta)
+}
+
 func main() {
+	http.HandleFunc("/enviar-texto", manejarEnviarcodigo)
+	http.Handle("/", http.FileServer(http.Dir(".")))
 	http.ListenAndServe("localhost:3000", nil)
 }
